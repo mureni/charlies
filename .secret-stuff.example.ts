2ab178f8c2b5287a0f3f9822253c3aa6f77b0071
RWDS <RWDS@RWDS.ORG>
2019-02-06 22:35:16 -0500

    cleaned up useless files

--- .secret-stuff.example.ts
+++ .secret-stuff.example.ts
@@ -4,4 +4,9 @@ export default token;
 
 // To invite the bot, replace the XXXXXXXXXX below with your client id number
 // from discordapp.com/developer and provide the full URL to an authorized user:
-// https://discordapp.com/oauth2/authorize?client_id=XXXXXXXXX&scope=bot&permissions=284736
+// https://discordapp.com/oauth2/authorize?client_id=XXXXXXXXX&scope=bot&permissions=67115008
+
+// quick reference for permissions values: 
+// send messages, send tts, change nick, read message history: 67180544
+// send messages, send tts, change nick, read message history, embed links: 67196928
+// send messages, send tts, change nick: 67115008
